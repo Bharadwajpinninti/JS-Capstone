2f7b173978170c98255601841d55c6cc5bebd545
Bharadwajpinninti <pinnintibharadwaj6@gmail,com>
2025-06-26 20:43:25 +0530

    update

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-const heading = document.getElementById("heading-2")
+let currentUser = true;
 
-console.log(heading);
+const sections = {
+    login: document.getElementById('login-section'),
+    signup: document.getElementById('signup-section')
+
+    // task
+    // notes
+};
+
+function renderHeader() {
+    const userInfo = document.getElementById("user-info");
+    const logoutButton = document.getElementById("logout-btn");
+    const desktopNavLinks = document.getElementById("desktop-nav");
+
+    if(currentUser === null) {
+        userInfo.textContent="";
+        logoutButton.style.display="none";
+        desktopNavLinks.querySelector("nav-dashboard").style.display="none"
+        desktopNavLinks.querySelector("#nav-tasks").style.display="none"
+        desktopNavLinks.querySelector("#nav-notes").style.display="none"
+    } else {
+        userInfo.textContent = "ranom@gmail.com";
+        logoutButton.style.display = "inline";
+        desktopNavLinks.querySelector("#nav-dashboard").style.display = "inline"
+        desktopNavLinks.querySelector("#nav-tasks").style.display = "inline"
+        desktopNavLinks.querySelector("#nav-notes").style.display = "inline"
+    }
+
+}
+
+renderHeader();
+
+function renderSignUp(){
+
+}
+
+renderSignUp()
+
+function renderLogIn() {
+
+}
+
+renderLogIn()
